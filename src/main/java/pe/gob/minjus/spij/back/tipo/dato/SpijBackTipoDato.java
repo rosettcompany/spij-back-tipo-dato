9c29160491c6d4c7d723700ac339725a72f293ff
Camila-Vertiz <camila.vrtz@gmail.com>
2023-06-08 11:23:26 -0500

    application

--- src/main/java/pe/gob/minjus/spij/back/tipo/dato/SpijBackTipoDato.java
+++ src/main/java/pe/gob/minjus/spij/back/tipo/dato/SpijBackTipoDato.java
@@ -3,32 +3,22 @@ package pe.gob.minjus.spij.back.tipo.dato;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"pe.gob.minjus.spij.back.tipo.dato.*"})
-public class SpijBackTipoDato extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(SpijBackTipoDato.class, args);
-    }
-    
-    @Override
+@ComponentScan(basePackages = { "pe.gob.minjus.spij.back.tipo.dato.*" })
+public class SpijBackTipoDato extends SpringBootServletInitializer {
+
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpijBackTipoDato.class);
-	}	
-    
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
-			}
-		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpijBackTipoDato.class, args);
 	}
 }
